031bd8c3cd27683cfb7ae4fa119cbf1c4681b686
AmineChannoufi <99873180+AmineChannoufi@users.noreply.github.com>
2022-04-08 16:34:46 +0200

    projet

---
+++ client/src/pages/Registration.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "../App.css";
+import { useNavigate } from "react-router";
+
+export default function Registration() {
+  const nav = useNavigate();
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
+
+  Axios.defaults.withCredentials = true;
+
+  const register = () => {
+    Axios.post("http://localhost:3001/register", {
+      name: usernameReg,
+      password: passwordReg,
+    })
+      .then((res) => {
+        setUsernameReg("");
+        setPasswordReg("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const login = async () => {
+    const { data } = await Axios.post("http://localhost:3001/login", {
+      name: username,
+      password: password,
+    });
+    if (data.message) {
+      setLoginStatus(data.message);
+    } else {
+      nav("/");
+    }
+
+    setUsername("");
+    setPassword("");
+  };
+
+  //request if the user session  exists
+  useEffect(() => {
+    Axios.get("http://localhost:3001/login").then((res) => {
+      if (res.data.loggedIn === true) {
+        setLoginStatus(res.data.user[0].name);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input
+          type="text"
+          value={usernameReg}
+          onChange={(e) => setUsernameReg(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          value={passwordReg}
+          onChange={(e) => setPasswordReg(e.target.value)}
+        />
+        <button onClick={register}> Register </button>
+      </div>
+
+      <div className="login">
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={login}> Login </button>
+      </div>
+
+      <h1>{loginStatus}</h1>
+    </div>
+  );
+}
--- server/index.js
+++ server/index.js
@@ -9,69 +9,81 @@ const session = require("express-session");
 const saltRounds = 10;
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:3000",
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
     credentials: true,
-})); // enable cors
+  })
+); // enable cors
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({key:'userID',secret: "secret", resave: false, saveUninitialized: false,cookie:{
-    expires: 60 * 60 * 24
-}}));
+app.use(
+  session({
+    key: "userID",
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24,
+    },
+  })
+);
 
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "0213",
+  password: "password",
   database: "usuarios",
 });
 
-app.get('/login', (req, res) => {
-    if(req.session.user){
-        res.send({loggedIn:true, user:req.session.user});
-    }else{
-        res.send({loggedIn:false});
-    }
-})
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
 
-app.post('/register', function(req, res) {
+app.post("/register", function (req, res) {
   const { name, password } = req.body;
-  bcrypt.hash(password,saltRounds,function(err,hash){
-
-    if(err){
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    if (err) {
       console.log(err);
     }
 
-    db.query(`INSERT INTO user (name, password) VALUES ('${name}','${hash}')`, function(error, results) {
-      if (error) throw error;
-      res.send(results);
-    });
-
-  })
-
-})
-
-app.post('/login', function(req, res) {
-  const { name, password } = req.body;
-  db.query(`SELECT * FROM user WHERE name = '${name}'`, function(error, results) {
-    if (error) throw error;
-    if(results.length > 0){
-      bcrypt.compare(password, results[0].password, function(err, result) {
-        if(result){
-          req.session.user = results
-          console.log(req.session.user);
-          res.send(results);
-        }else{
-          res.send({message:"error, user or password incorrect"});
-        }
-      })
-    }else{
-      res.send({message: 'user does not exist'});
-    }
+    db.query(
+      `INSERT INTO user (name, password) VALUES ('${name}','${hash}')`,
+      function (error, results) {
+        if (error) throw error;
+        res.send(results);
+      }
+    );
   });
-})
+});
+
+app.post("/login", function (req, res) {
+  const { name, password } = req.body;
+  db.query(
+    `SELECT * FROM user WHERE name = '${name}'`,
+    function (error, results) {
+      if (error) throw error;
+      if (results.length > 0) {
+        bcrypt.compare(password, results[0].password, function (err, result) {
+          if (result) {
+            req.session.user = results;
+            res.status(200).send(results);
+          } else {
+            res.send({ message: "error, user or password incorrect" });
+          }
+        });
+      } else {
+        res.send({ message: "user does not exist" });
+      }
+    }
+  );
+});
 
 app.listen(3001, () => {
   console.log("running server");
